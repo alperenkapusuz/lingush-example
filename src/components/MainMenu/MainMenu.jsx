46f65301f11ab177abec1772bd69f9bb694202b1
alperenkapusuz <sefalperen21@gmail.com>
2022-06-25 19:57:53 +0300

    Carousel yapildi

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,22 +1,94 @@
 import React from "react";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import turtle from "../../images/mainmenu/turtle2.png";
+import g1 from "../../images/whyLingush/g1.png";
+import g2 from "../../images/whyLingush/g2.png";
+import g3 from "../../images/whyLingush/g3.png";
+import g4 from "../../images/whyLingush/g4.png";
+import g5 from "../../images/whyLingush/g5.png";
+import g6 from "../../images/whyLingush/g6.png";
 import "./MainMenu.css";
 
 function MainMenu() {
   return (
     <section className="mainmenu-page">
       {/*  Hosgeldiniz  */}
-      <div className="mainmenu-welcome">
-        <div className="welcome-div-img">
-          <img className="welcome-img" src={turtle} alt="turtle" />
-        </div>
-        <div className="welcome-p">
-          Çocuğunuza etkileşimli bir ortam sağlayarak yeni kelimeleri eğlenceli
-          ve etkili bir şekilde öğretmenin yolu
-        </div>
-      </div>
+      <Container className="mainmenu-welcome">
+        <Row>
+          <Col className="welcome-div-img">
+            <img className="welcome-img" src={turtle} alt="turtle" />
+          </Col>
+          <Col className="welcome-p">
+            Çocuğunuza etkileşimli bir ortam sağlayarak yeni kelimeleri
+            eğlenceli ve etkili bir şekilde öğretmenin yolu
+          </Col>
+        </Row>
+      </Container>
+      <hr/>
       {/* Neden lingush */}
-      <div className="mainmenu-why-lingush"></div>
+      <Container className="mainmenu-why-lingush">
+        <Carousel className="mainmenu-carousel" fade variant="dark">
+          <Carousel.Item>
+            <img src={g1} alt="First slide" />
+            <Carousel.Caption>
+              <h3>REKABETÇİ OLALIM</h3>
+              <p>
+                Nerede durduğunuzu öğrenin, sınavı en çok alan üç kişi arasında
+                yer alın
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={g2} alt="Second slide" />
+
+            <Carousel.Caption>
+              <h3>AR İLE GÜÇLENDİRİLMİŞ ETKİLEŞİMLER</h3>
+              <p>
+                AR, çocuklarınız yeni kelimeler öğrenirken evde aktif
+                kalmalarını sağlayacak
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={g3} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>REKABETÇİ ORTAM</h3>
+              <p>
+                Çocuğunuzun haftalık ilerlemesini, raporlarını ve analizlerini
+                alacaksınız
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={g4} alt="First slide" />
+            <Carousel.Caption>
+              <h3>EN İYİ HAFIZA OYUNU</h3>
+              <p>Birden çok seviyeli hafıza oyunuyla hafızanızı güçlendirin</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={g5} alt="First slide" />
+            <Carousel.Caption>
+              <h3>YILLIK ABONELİKLE PREMİUM'A GİT</h3>
+              <p>
+                Lingush'un sunduğu tüm üstün özellikleri sadece yıllık $ 100
+                abonelikle alın
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={g5} alt="First slide" />
+            <Carousel.Caption>
+              <h3>İNTERAKTİF OYUNLAR</h3>
+              <p>
+                Çocuklarınızla oynayın, Çocuklarınızla evde Nesneleri Gizle ve
+                Bul oynamayı seveceksiniz
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
       {/* lingush uygulamasi  */}
       <div className="mainmenu-lingush-app"></div>
       {/* Satin alin  */}
--- src/components/Navbar/Navi.jsx
+++ src/components/Navbar/Navi.jsx
@@ -1,39 +1,3 @@
-// import React, { useRef } from "react";
-// import { FaBars, FaTimes } from "react-icons/fa";
-// import logo from "../../images/navbar/logo.png";
-// import "./Navi.css";
-
-// function Navi() {
-//   const navRef = useRef();
-//   const showNavbar = () => {
-//     navRef.current.classList.toggle("responsive_nav");
-//   };
-//   return (
-//     <header>
-//       <div className="lingush-div-logo-name">
-//         <img className="lingush-logo" src={logo} alt="lingush-logo" />
-//         <h3 className="lingush-name">LINGUSH</h3>
-//       </div>
-//       <nav ref={navRef}>
-//         <label className="label-language">Site Dili:</label>
-//         <select name="language" id="language">
-//           <option value="Turkce">Türkçe</option>
-//           <option value="Ingilizce">Ingilizce</option>
-//         </select>
-//         <button className="login-button">GİRİŞ</button>
-//         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-//           <FaTimes />
-//         </button>
-//       </nav>
-//       <button className="nav-btn" onClick={showNavbar}>
-//         <FaBars />
-//       </button>
-//     </header>
-//   );
-// }
-
-// export default Navi;
-
 import React from "react";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../../images/navbar/logo.png";
