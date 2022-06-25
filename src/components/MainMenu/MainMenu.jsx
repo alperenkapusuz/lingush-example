34c4c98462a8b47bc7c93b57962a29c1dec47378
alperenkapusuz <sefalperen21@gmail.com>
2022-06-25 18:06:09 +0300

    Navi degistirildi

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -4,26 +4,26 @@ import "./MainMenu.css";
 
 function MainMenu() {
   return (
-    <div>
+    <section className="mainmenu-page">
       {/*  Hosgeldiniz  */}
       <div className="mainmenu-welcome">
-        <div className="welcome-p">
-            Çocuğunuza etkileşimli bir ortam sağlayarak yeni kelimeleri
-            eğlenceli ve etkili bir şekilde öğretmenin yolu
-        </div>
         <div className="welcome-div-img">
           <img className="welcome-img" src={turtle} alt="turtle" />
+        </div>
+        <div className="welcome-p">
+          Çocuğunuza etkileşimli bir ortam sağlayarak yeni kelimeleri eğlenceli
+          ve etkili bir şekilde öğretmenin yolu
         </div>
       </div>
       {/* Neden lingush */}
       <div className="mainmenu-why-lingush"></div>
-       {/* lingush uygulamasi  */}
+      {/* lingush uygulamasi  */}
       <div className="mainmenu-lingush-app"></div>
       {/* Satin alin  */}
       <div className="mainmenu-buy"></div>
       {/* Footer  */}
       <div className="mainmenu-footer"></div>
-    </div>
+    </section>
   );
 }
 
--- src/components/Navbar/Navi.jsx
+++ src/components/Navbar/Navi.jsx
@@ -1,35 +1,75 @@
-import React, { useRef } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+// import React, { useRef } from "react";
+// import { FaBars, FaTimes } from "react-icons/fa";
+// import logo from "../../images/navbar/logo.png";
+// import "./Navi.css";
+
+// function Navi() {
+//   const navRef = useRef();
+//   const showNavbar = () => {
+//     navRef.current.classList.toggle("responsive_nav");
+//   };
+//   return (
+//     <header>
+//       <div className="lingush-div-logo-name">
+//         <img className="lingush-logo" src={logo} alt="lingush-logo" />
+//         <h3 className="lingush-name">LINGUSH</h3>
+//       </div>
+//       <nav ref={navRef}>
+//         <label className="label-language">Site Dili:</label>
+//         <select name="language" id="language">
+//           <option value="Turkce">Türkçe</option>
+//           <option value="Ingilizce">Ingilizce</option>
+//         </select>
+//         <button className="login-button">GİRİŞ</button>
+//         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+//           <FaTimes />
+//         </button>
+//       </nav>
+//       <button className="nav-btn" onClick={showNavbar}>
+//         <FaBars />
+//       </button>
+//     </header>
+//   );
+// }
+
+// export default Navi;
+
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../../images/navbar/logo.png";
 import "./Navi.css";
 
-function Navi() {
-  const navRef = useRef();
-  const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav");
-  };
+const Navi = () => {
   return (
-    <header>
-      <div className="lingush-div-logo-name">
-        <img className="lingush-logo" src={logo} alt="lingush-logo" />
-        <h3 className="lingush-name">LINGUSH</h3>
-      </div>
-      <nav ref={navRef}>
-        <label className="label-language">Site Dili:</label>
-        <select name="language" id="language">
-          <option value="Turkce">Türkçe</option>
-          <option value="Ingilizce">Ingilizce</option>
-        </select>
-        <button className="login-button">GİRİŞ</button>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
-        </button>
-      </nav>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
-    </header>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      variant="dark"
+      sticky="top"
+      className="navbar-page"
+    >
+      <Container>
+        <img src={logo} alt="lingush-logo" className="lingush-logo"/>
+        <Navbar.Brand className="navbar-name">LINGUSH</Navbar.Brand>
+        <Navbar.Toggle
+          className="navbar-toggle"
+          aria-controls="responsive-navbar-nav"
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto"></Nav>
+          <Nav>
+            <NavDropdown title="Site Dili" id="collasible-nav-dropdown">
+              <NavDropdown.Item>Türkçe</NavDropdown.Item>
+              <NavDropdown.Item>Ingilizce</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <button className="login-button">GİRİŞ</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-}
+};
 
 export default Navi;
