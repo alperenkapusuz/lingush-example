175abdf769e2809ebe274bd190d0a9f48223961f
alperenkapusuz <sefalperen21@gmail.com>
2022-06-27 22:35:55 +0300

    sign up sayfasi tamamlandi

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -7,7 +7,7 @@ const SignUp = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
-  const { signup,currentUser } = useAuth();
+  const { signup } = useAuth();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
@@ -33,7 +33,6 @@ const SignUp = () => {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
-          {currentUser.email && currentUser.email}
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
